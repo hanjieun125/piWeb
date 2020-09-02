d259fb72afc1b5cc4904e52a60743fe3c6cbe6a4
hanjieun125 <hanjieun125@naver.com>
2020-09-02 16:14:56 +0900

    'aaa'

---
+++ start_flaskcrud.py
@@ -0,0 +1,10 @@
+from flaskcrud import app
+
+<<<<<<< HEAD
+if __name__ == "__main__":
+    app.run(debug=True)
+
+=======
+if __name__ == '__main__':
+    app.run(debug=True)
+>>>>>>> d8b2d830b0d485e921adebe1783698e47dfdd1e1c
